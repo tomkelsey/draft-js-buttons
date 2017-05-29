ae07a118be7fa84217f54647a79d955e4ee5c22e
tomkelsey <tom@kelsey.me>
2017-05-29 12:25:42 +0100

    Ability to edit existing links

--- src/utils/createLinkButton.js
+++ src/utils/createLinkButton.js
@@ -1,14 +1,54 @@
 import React, { Component } from 'react';
 import { RichUtils, EditorState } from 'draft-js';
 
+function getCurrentEntityKey(editorState) {
+  const selection = editorState.getSelection();
+  const anchorKey = selection.getAnchorKey();
+  const contentState = editorState.getCurrentContent();
+  const anchorBlock = contentState.getBlockForKey(anchorKey);
+  const offset = selection.anchorOffset;
+  const index = selection.isBackward ? offset - 1 : offset;
+  return anchorBlock.getEntityAt(index);
+}
+
+function getCurrentEntity(editorState) {
+  const contentState = editorState.getCurrentContent();
+  const entityKey = getCurrentEntityKey(editorState);
+  if (entityKey) {
+    return contentState.getEntity(entityKey);
+  }
+  return null;
+}
+
+function hasEntity(entityType, editorState) {
+  const entity = getCurrentEntity(editorState);
+  if (entity && entity.getType() === entityType) {
+    return true;
+  }
+  return false;
+}
+
 export default ({ children }) => (
   class linkButton extends Component {
 
     activate = (event) => {
       event.preventDefault();
       event.stopPropagation();
-      const url = window.prompt('Enter/Paste URL...');
+
       const editorState = this.props.getEditorState();
+
+      let url = '';
+      const entitySelected = hasEntity('link', editorState);
+      if (entitySelected) {
+        const entity = getCurrentEntity(editorState);
+        if (entity) {
+          const data = entity.getData();
+          url = data.url;
+        }
+      }
+
+      url = window.prompt('Enter/Paste URL...', url);
+
       if (url) {
         const contentState = editorState.getCurrentContent();
         const contentStateWithEntity = contentState.createEntity('link', 'IMMUTABLE', { url });
